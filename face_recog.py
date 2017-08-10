c768be32a949149bb75a795f7769dd2a8910b32c
weiy1991 <weiy1991@163.com>
2017-08-10 18:29:51 +0800

    add the time cost calculation for each frame detection

--- face_recog.py
+++ face_recog.py
@@ -8,15 +8,7 @@ import face_recognition
 import cv2
 import csv
 import numpy as np
-
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
-
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+from datetime import datetime
 
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
@@ -69,6 +61,9 @@ face_names = []
 process_this_frame = True
 
 while True:
+
+    time_start = datetime.now()  #get the start time
+
     # Grab a single frame of video
     ret, frame = video_capture.read()
 
@@ -134,6 +129,11 @@ while True:
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+    time_end = datetime.now()  #get the end time
+
+    time_cost = time_end - time_start
+    print("time cost for detection:", (time_cost.microseconds / 1000.0) )
 
 # Release handle to the webcam
 video_capture.release()
